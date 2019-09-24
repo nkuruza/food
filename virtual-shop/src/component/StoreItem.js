1f1bab49e368f9ebf3de9933d1d2878da8764cb2
Nkululeko Mthethwa <nkuruza@gmail.com>
2019-09-24 11:17:13 +0200

    Migration To Expo
    Lots of errors

---
+++ virtual-shop/src/component/StoreItem.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text, View, TouchableOpacity } from 'react-native';
+import styles from '../style.js';
+
+export default class StoreItem extends React.PureComponent {
+    _onPress = () => {
+        this.props.onPressItem(this.props.item);
+    }
+    render(){
+        return (
+            <TouchableOpacity onPress={this._onPress} style={styles.product}>
+                <View style={{ alignItems: "center" }}>
+                    <Text>{this.props.title}</Text>
+                </View>
+            </TouchableOpacity>
+        );
+    }
+}
